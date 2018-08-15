fc14b3501ba2a8918a1238eaabc9f8dfe4db6b6e
chenpiaopiao <chenpiaopiao@lightinthebox>
2018-08-15 18:35:59 +0800

    replace train model show each epoch calculate time

--- captcha_train_gpu_plot.py
+++ captcha_train_gpu_plot.py
@@ -6,7 +6,7 @@ import my_dataset
 from captcha_cnn_model import CNN
 import matplotlib.pyplot as plt
 import captcha_test
-import threading
+import time
 
 # Hyper Parameters
 num_epochs = 30
@@ -22,8 +22,11 @@ plt.ion()
 
 def draw_line(xv):
     rate = captcha_test.calulate_rate()
-    x.append(xv/100)
+    x.append(xv)
     y.append(rate)
+    plt.cla()
+    plt.plot(x, y)
+    plt.pause(0.1)
 
 
 def main():
@@ -35,38 +38,24 @@ def main():
     # Train the Model
     train_dataloader = my_dataset.get_train_data_loader()
     for epoch in range(num_epochs):
+        time_time = time.time()
         for i, (images, labels) in enumerate(train_dataloader):
             images = Variable(images).cuda()
             labels = Variable(labels.float()).cuda()
             predict_labels = cnn(images)
-            # print(predict_labels.type)
-            # print(labels.type)
             loss = criterion(predict_labels, labels)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            if (i + 1) % 10 == 0:
-                print("epoch:", epoch, "step:", i, "loss:", loss.item())
-            if (i + 1) % 100 == 0:
-                torch.save(cnn.state_dict(), "./model.pkl")  # current is model.pkl
-                print("save model")
-                try:
-                    plt.cla()
-                    plt.plot(x, y)
-                    plt.pause(0.1)
-                except:
-                    pass
-                threading.Thread(target=draw_line, args=[i]).start()
-                # plt.plot(y, x)
-                # plt.show()
-        # torch.save(cnn.state_dict(), "./model.pkl")  # current is model.pkl
-        # print("save model")
-        # rate = captcha_test.calulate_rate()
-        # x.append(epoch)
-        # y.append(rate)
-        # plt.plot(x)
-        # plt.axis(y)
-        # plt.show()
+            # if (i + 1) % 10 == 0:
+            #     print("epoch:", epoch, "step:", i, "loss:", loss.item())
+            # if (i + 1) % 100 == 0:
+            #     torch.save(cnn.state_dict(), "./model.pkl")  # current is model.pkl
+            #     print("save model")
+        print("epoch %d %.2fs" % (epoch, time.time() - time_time))
+        torch.save(cnn.state_dict(), "./model.pkl")  # current is model.pkl
+        print("save model")
+        draw_line(epoch)
     torch.save(cnn.state_dict(), "./model.pkl")  # current is model.pkl
     print("save last model")
 
